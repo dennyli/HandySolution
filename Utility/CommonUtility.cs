ff376b1f364bcf1b935d009032be82359847f1cb
dennyli <denny_001@163.com>
2015-07-01 16:55:11 +0800

    Add Lighter.Server.Infrastructure; Modify Lighter.Service; Add lib: log4net & Newtonsoft.Json

--- Utility/CommonUtility.cs
+++ Utility/CommonUtility.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Reflection;
+using System.IO;
 
 namespace Utility
 {
@@ -22,6 +24,53 @@ namespace Utility
             sb.Insert(0, "添加记录遇到错误: \n");
 
             return sb.ToString();
+        }
+
+        private static bool DllIsNetBased(string dllName)
+        {
+            try
+            {
+                AssemblyName testAssembly = AssemblyName.GetAssemblyName(dllName);
+                return true;
+            }
+            catch
+            {
+            }
+
+            return false;
+        }
+
+        public static IList<Assembly> LookupAssemblies(string path, Type serviceIterface)
+        {
+            IList<Assembly> ass = new List<Assembly>();
+            try
+            {
+                var dlls = from dll in Directory.EnumerateFiles(path, "*.dll") select dll;
+                foreach (var dll in dlls)
+                {
+                    if (DllIsNetBased(dll))
+                    {
+                        Assembly assembly = Assembly.LoadFrom(dll);
+                        foreach (Type type in assembly.GetTypes())
+                        {
+                            if (type.IsSubclassOf(serviceIterface))
+                            {
+                                ass.Add(assembly);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException UAEx)
+            {
+                throw new Exception("LookupCommand: " + UAEx.Message);
+            }
+            catch (PathTooLongException PathEx)
+            {
+                throw new Exception("LookupCommand: " + PathEx.Message);
+            }
+
+            return ass;
         }
     }
 }
